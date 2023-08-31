8bf86e7e55a390094c356f9c650370517ea6a547
shahriar-programmer <shovonshahriar01@gmail.com>
2023-08-31 23:12:35 +0600

    Made responsive

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ const canvas = document.querySelector("canvas"),
     colorPicker = document.querySelector("#color-picker"),
     clearCanvas = document.querySelector(".clear"),
     saveImg = document.querySelector(".save"),
-    ctx = canvas.getContext("2d");
+    ctx = canvas.getContext("2d"),
+    hamburgerMenu = document.getElementById("hamburger"),
+    hamburgerIcon1 = document.getElementById("hamburger-icon-1"),
+    hamburgerIcon2 = document.getElementById("hamburger-icon-2"),
+    toolsBoard = document.getElementById("tools-board");
 
 let pervMouseX, pervMouseY, snapshot,
     isDrawing = false,
     brushwidth = 5,
     selectedTool = "brush",
     selectedColor = "#000";
-
 
 window.addEventListener("load", () => {
     canvas.height = canvas.offsetHeight;
@@ -37,11 +40,20 @@ const drawRect = (e) => {
 }
 
 const drawing = (e) => {
+
+    // Prevent Default to stop scrolling on touches.
+    e.preventDefault();
+    
     if (!isDrawing) return;
+
+    // For Mobile
+    const rect = e.target.getBoundingClientRect();
+    const mobileOffsetX = e?.targetTouches?.[0].clientX - rect.x;
+    const mobileOffsetY = e?.targetTouches?.[0].clientY - rect.y;
 
     if (selectedTool === "brush" || selectedTool === "eraser") {
         ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
-        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.lineTo(e.offsetX || mobileOffsetX, e.offsetY || mobileOffsetY);
         ctx.stroke();
         //console.log(selectedTool);
     }
@@ -84,6 +96,17 @@ saveImg.addEventListener("click", () => {
     link.click();
 })
 
-canvas.addEventListener("pointerdown", startDraw);
-canvas.addEventListener("pointermove", drawing);
-canvas.addEventListener("pointerup", () => isDrawing = false);
+canvas.addEventListener("mousedown", startDraw);
+canvas.addEventListener("mousemove", drawing);
+canvas.addEventListener("mouseup", () => isDrawing = false);
+
+// For Mobile Touches
+canvas.addEventListener("touchstart", startDraw);
+canvas.addEventListener("touchmove", drawing);
+canvas.addEventListener("touchend", () => isDrawing = false);
+
+hamburgerMenu.addEventListener("click", () => {
+    toolsBoard.classList.toggle("tools-board-closed")
+    hamburgerIcon1.classList.toggle("hamburger-icon-1-closed")
+    hamburgerIcon2.classList.toggle("hamburger-icon-2-closed")
+})
